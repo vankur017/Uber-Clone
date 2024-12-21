8476f6ae3057205be4cbf513d4b61b811c901b1a
unknown <vankur017@gmail.com>
2024-12-21 11:38:06 +0530

    user logged out route

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,7 @@ dotenv.config();
 
 const cors = require('cors');
 const express = require('express');
+const cookieParser = require('cookie-parser');
 const connectToDb = require('./db/db');
 const userRoutes = require('./routes/user.routes');
 
@@ -13,6 +14,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     res.send('Hello World');
